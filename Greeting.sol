45b9fef830ace73c2cd7a1cdd16ab32a6a3abc07
jg8481 <joshua.gorospe@gmail.com>
2023-06-23 23:41:54 -0400

    Create Greeting.sol

---
+++ Greeting.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+contract HelloWorld {
+  string private greeting;
+  uint public version = 0;
+  
+  constructor (string memory _greeting) {
+    greeting = _greeting;
+  }
+
+  function greet() public view returns(string memory) {
+    return greeting;
+  }
+}
